86e0425ef21b17ea36f8875bdf1894cf1454ccec
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-05 20:03:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,5 @@
-# Add  code here!
+require 'prime'
 def prime?(num)
-  if (2..num-1).any? {|num2|num % num2 == 0} == true
-    false
-  else true
-  end
+  #(2..num-1).any? {|num2|num % num2 == 0}
+  num.prime?
 end
